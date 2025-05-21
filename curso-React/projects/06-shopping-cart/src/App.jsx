4265d72e5254fb7b9beefc0e7ebd9760593da531
araldev <arturo.r2d2.dev@gmail.com>
2025-05-21 19:48:32 +0200

    update course

--- curso-React/projects/06-shopping-cart/src/App.jsx
+++ curso-React/projects/06-shopping-cart/src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
+import { Header } from './components/Header.jsx'
+import { Products } from './components/Products.jsx'
+import { Footer } from './components/Footer.jsx'
+import { useFilters } from './hooks/useFilters.js'
 import { products as initialProducts } from './mocks/products.json'
-import { Products } from './components/Products'
-import { Header } from './components/Header'
+import { IS_DEVELOPMENT } from './conifg.js'
 
 export default function App () {
-  const [products, setProducts] = useState(initialProducts)
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-        product.price >= filters.minPrice &&
-        (
-          filters.category === 'all' ||
-          product.category === filters.category
-        )
-      )
-    })
-  }
-
+  const [products] = useState(initialProducts)
+  const { filters, filterProducts, setFilters } = useFilters()
   const filteredProducts = filterProducts(products)
 
   return (
     <>
       <Header setFilters={setFilters} />
       <Products products={filteredProducts} />
+      {IS_DEVELOPMENT && <Footer filters={filters} />}
     </>
   )
 }
